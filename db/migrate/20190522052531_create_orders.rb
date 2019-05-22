ea487d37efdfba483ed4d1f8cd14b6b986990262
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-05-22 00:32:33 -0500

    rename aasm column

--- db/migrate/20190522052531_create_orders.rb
+++ db/migrate/20190522052531_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.string :seller_name
-      t.string :status
+      t.string :aasm_state
 
       t.timestamps
     end
