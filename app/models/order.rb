7bded59d3408d7743865c0cd7658836f42e7c50e
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-05-22 00:30:33 -0500

    implement aasm

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  include AASM
+
+  state :ordered, initial: true
+  state :returned, :resent, :distribution, :cancelled, :completed
+
+  event :send_again do
+    transitions from: [:returned, :cancelled], to: :distribution
+  end
+
+  event :cancel_order do
+    transitions from: :ordered, to: :cancelled
+  end
 end
