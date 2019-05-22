eb4f8f9abb8d84be048984a14ecf1364615d65b0
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-05-22 00:52:32 -0500

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_052531) do
 
   create_table "orders", force: :cascade do |t|
     t.string "seller_name"
-    t.string "status"
+    t.string "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
