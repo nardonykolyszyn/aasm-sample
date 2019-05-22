c85cb988dc3fbf450743ea486ca17c7a2346b838
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-05-22 01:03:58 -0500

    comment possible events

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,13 +7,13 @@ class Order < ApplicationRecord
   aasm do
     state :ordered, initial: true
     state :returned, :resent, :distribution, :cancelled, :completed
-
-    event :send_again do
-      transitions from: [:returned, :cancelled], to: :distribution
-    end
-
-    event :cancel_order do
-      transitions from: :ordered, to: :cancelled
-    end
+    # Possible events
+    # event :send_again do
+    #   transitions from: [:returned, :cancelled], to: :distribution
+    # end
+    # 
+    # event :cancel_order do
+    #   transitions from: :ordered, to: :cancelled
+    # end
   end
 end
